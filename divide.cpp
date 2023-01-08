2bfa2bab1a8c4ab7b039af3a5e2e8c8f06a0f372
Ashish Yadav <ashishyadav88203@gmail.com>
2023-01-09 01:03:15 +0530

    add file

--- divide.cpp
+++ divide.cpp
@@ -5,5 +5,9 @@ int main()
 	int a,b;
 	cout<<"Enter the value of a and b: ";
 	cin>>a>>b;
+<<<<<<< HEAD
 	cout<<"The divide is is : "<<(a/b);
+=======
+	cout<<"The sub is : "<<(a-b);
+>>>>>>> feature1
 }
